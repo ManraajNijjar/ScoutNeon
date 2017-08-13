225d96341128aa815ad7effdefbdd8b288e31b72
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-08-13 11:34:43 -0400

    Initial Commit with Twitter authentication set up

---
+++ ScoutNeon/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ScoutNeon
+//
+//  Created by Manraaj Nijjar on 8/9/17.
+//  Copyright © 2017 Manraaj Nijjar. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+            if (session != nil) {
+                print("signed in as \(session?.userName)");
+            } else {
+                print("error: \(error?.localizedDescription)");
+            }
+        })
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
