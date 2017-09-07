0a1151de158c8beeb0d325b07403535a98b838ed
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-09-06 22:31:36 -0400

    Added text validation to the post creator as well as changing the textView to a textField

--- ScoutNeon/NewPostViewController.swift
+++ ScoutNeon/NewPostViewController.swift
@@ -15,11 +15,13 @@ class NewPostViewController: UIViewController {
     
     @IBOutlet weak var titleTextField: UITextField!
     
-    @IBOutlet weak var messageTextField: UITextView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
     
     @IBOutlet weak var submitButton: UIButton!
     
     let firebaseController = FirebaseController.sharedInstance()
+    let validator = TextValidationController.sharedInstance()
     
     var color: UIColor!
     var userProfile: Profile!
@@ -28,6 +30,7 @@ class NewPostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        submitButton.isEnabled = false
         let geocoder = CLGeocoder()
         geocoder.reverseGeocodeLocation(CLLocation(latitude: postLatitude, longitude: postLongitude)) { (placemarkArray, error) in
             if error == nil {
@@ -41,7 +44,23 @@ class NewPostViewController: UIViewController {
     
     
     @IBAction func submitButtonPressed(_ sender: Any) {
-        firebaseController.newPost(username: userProfile.username!, topicTitle: titleTextField.text!, topicMessage: messageTextField.text, color: color.hexCode, latitude: postLatitude, longitude: postLongitude)
+        firebaseController.newPost(username: userProfile.username!, topicTitle: titleTextField.text!, topicMessage: messageTextField.text!, color: color.hexCode, latitude: postLatitude, longitude: postLongitude)
     }
+    
+    @IBAction func titleFieldChanged(_ sender: Any) {
+        validateText()
+    }
+    
+    @IBAction func messageFieldChanged(_ sender: Any) {
+        validateText()
+    }
+    
 
+    func validateText() {
+        if validator.validator.validateString(titleTextField.text!) && validator.validator.validateString(messageTextField.text!) {
+            submitButton.isEnabled = true
+        } else {
+            submitButton.isEnabled = false
+        }
+    }
 }
