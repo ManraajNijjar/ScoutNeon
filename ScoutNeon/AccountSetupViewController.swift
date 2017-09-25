9921114a1505e4ae6ebbbf6a97b3e188f538bd43
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-09-24 20:31:56 -0400

    Added Indepth comments to the AccountSetupView

--- ScoutNeon/AccountSetupViewController.swift
+++ ScoutNeon/AccountSetupViewController.swift
@@ -34,9 +34,11 @@ class AccountSetupViewController: UIViewController {
     var editColor: UIColor!
     var firstEdit = true
     
+    //Pulled from the Login View Controller
     var userIDFromLogin: String!
     
     var firebaseIDFromLogin: String!
+    
     
     var colorPicker = ChromaColorPicker()
     
@@ -57,13 +59,16 @@ class AccountSetupViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Removes the background Color if the device is an iPad
         switch UIDevice.current.userInterfaceIdiom {
         case .pad:
             backgroundColorView.isHidden = true
             
         default: print("Not pad")
+            
         }
-        //Disables the submit button at the start of account settings creation
+        
+        //Sets up the blur effect to the photo and moves it to the back
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
         blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = self.view.bounds
@@ -72,20 +77,26 @@ class AccountSetupViewController: UIViewController {
         view.addSubview(blurEffectView)
         submitButton.isEnabled = false
         
+        //Sets up the Chroma Color Picker and connects two methods to the actions you can use with it
         colorPicker = setupChromaColorPicker()
         colorPicker.addTarget(self, action: #selector(AccountSetupViewController.colorSliderMoved), for: .touchUpInside)
         colorPicker.shadeSlider.addTarget(self, action: #selector(AccountSetupViewController.colorSliderMoved), for: .touchUpInside)
 
         view.addSubview(colorPicker)
-
+        
+        //Turns the Image and the Background Color View into a circle
         profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
         profileImage.clipsToBounds = true
         backgroundColorView.layer.cornerRadius = profileImage.frame.size.width / 2
         backgroundColorView.clipsToBounds = true
+        
+        //Sends the subviews to the back of the View in order to make everything function properly
         view.sendSubview(toBack: backgroundColorView)
         view.sendSubview(toBack: blurEffectView)
         view.sendSubview(toBack: backgroundImage)
         
+        
+        //Begins to retrieve the users image from Twitter
         activityIndicator.startAnimating()
         twitterAPI.getImageForUserID(userID: userIDFromLogin, size: "Large") { (image) in
             DispatchQueue.main.async {
@@ -95,7 +106,8 @@ class AccountSetupViewController: UIViewController {
                 case .phone:
                     self.profileImage.image = image
                 case .pad:
-                    // It's an iPad
+                    
+                    //Scales the image for iPad sizes
                     let screenSize: CGRect = UIScreen.main.bounds
                     let selectedSize = screenSize.width/2.5
                     self.profileImage.image = self.ResizeImage(image: image!, targetSize: CGSize(width: selectedSize, height: selectedSize))
@@ -107,9 +119,13 @@ class AccountSetupViewController: UIViewController {
                 }
             }
         }
+        
+        //Moves the usernameTextField to the front
         view.bringSubview(toFront: usernameTextField)
         
         usernameTextField.delegate = self
+        //If the view is in editmode and not login changes teh values accordinglty
+        
         if editmode {
             usernameTextField.text = editProfile.username
             colorPicker.adjustToColor(editColor)
@@ -118,6 +134,7 @@ class AccountSetupViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(false)
+        //Makes the view change in the background as a transition effect
         UIView.animate(withDuration: 0.5, animations: {
             self.view.backgroundColor = UIColor.white
             self.blurEffectView.alpha = 0.5
@@ -125,6 +142,7 @@ class AccountSetupViewController: UIViewController {
     }
     
     
+    //The function called by the interactions with the Chroma Elements
     func colorSliderMoved() {
         backgroundColorView.backgroundColor = colorPicker.currentColor
         colorAPI.getColorNameByHex(selectColor: colorPicker.currentColor) { (results, error) in
@@ -145,9 +163,11 @@ class AccountSetupViewController: UIViewController {
             firstEdit = false
         }
         
+        
         if validator.validator.validateString(potentialName!) {
             //Could move this part to when submit is presed if the app is connecting to Firebase too often
             activityIndicator.startAnimating()
+            //Checks to see if the username is already taken
             fireBaseController.userExists(userId: potentialName!, baseView: self, userExistsCompletionHandler: { (userStatus) in
                 self.submitButton.isEnabled = !(userStatus)
                 self.activityIndicator.stopAnimating()
@@ -160,15 +180,14 @@ class AccountSetupViewController: UIViewController {
     @IBAction func submitPressed(_ sender: Any) {
         
         let reachability = Reachability()!
+        //Determines if a user already exists so as to edit the profile rather than make a new one
         if editmode {
-            
             reachability.whenReachable = { reachability in
                 DispatchQueue.main.async {
                     self.editProfile.color = self.colorPicker.currentColor.hexCode
                     self.editProfile.username = self.usernameTextField.text!
-                    
                     CoreDataController.saveContext()
-                    
+                    //Pushes the new profile to the Firebase database
                     self.fireBaseController.createUser(userProfile: self.editProfile, baseView: self)
                     self.performSegue(withIdentifier: "MapSegue", sender: self)
                 }
@@ -211,13 +230,13 @@ class AccountSetupViewController: UIViewController {
         
     }
     
+    //Takes an image and resizes it
     func ResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
         let size = image.size
         
         let widthRatio  = targetSize.width  / image.size.width
         let heightRatio = targetSize.height / image.size.height
         
-        // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
             newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
@@ -225,10 +244,8 @@ class AccountSetupViewController: UIViewController {
             newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
         }
         
-        // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
         
-        // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
         image.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
@@ -245,6 +262,7 @@ class AccountSetupViewController: UIViewController {
         }
     }
     
+    //Unwind segue for the Map View when entering Edit Mode
     @IBAction func unwindToAccountView(segue:UIStoryboardSegue) {
         
     }
@@ -293,22 +311,6 @@ extension AccountSetupViewController: ChromaColorPickerDelegate {
             return colorPicker
         default: print("Case not Found")
         }
-        /*
-        //Sets up the chroma color picker
-        let sizeValue = view.frame.size.width * 0.586
-        let colorPicker = ChromaColorPicker(frame: CGRect(x: 0, y: 0, width: sizeValue, height: sizeValue))
-        colorPicker.delegate = self
-        colorPicker.padding = 5
-        colorPicker.stroke = 3
-        colorPicker.hexLabel.textColor = UIColor.black
-        //Places it in the center of the view should likely implement a place to put it so it has constraints
-        //neatColorPicker.center = self.colorPickerView.center
-        
-        //colorPicker.center = self.view.center
-        let sizeHeight = view.frame.size.height * 0.1877
-        //Closest equivalent to 110 on iPhone7
-        colorPicker.center = CGPoint(x: self.view.center.x, y: self.view.center.y + sizeHeight)
-        return colorPicker */
         return ChromaColorPicker()
     }
     
@@ -320,6 +322,7 @@ extension AccountSetupViewController: ChromaColorPickerDelegate {
     
     
 }
+//Makes it so the TextFields close the keyboard on return
 extension AccountSetupViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
