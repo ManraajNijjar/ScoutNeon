bff833c3ba7dd775b6bc8599d862365e7b4801c9
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-10-10 19:32:07 -0400

    Cleanup: Cleaned up the New Post View Controller

--- ScoutNeon/NewPostViewController.swift
+++ ScoutNeon/NewPostViewController.swift
@@ -14,21 +14,18 @@ import ReachabilitySwift
 class NewPostViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
-    
     @IBOutlet weak var titleTextField: UITextField!
-    
     @IBOutlet weak var messageTextField: UITextField!
-    
-    
     @IBOutlet weak var submitButton: UIButton!
-    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     let firebaseController = FirebaseController.sharedInstance
     let validator = TextValidationController.sharedInstance
     let errorAlertController = ErrorAlertController()
-    var reachability = Reachability()!
     
+    var reachabilityNetworkConnection = Reachability()!
+    
+    //Global Variables
     var color: UIColor!
     var userProfile: Profile!
     var postLongitude:Double!
@@ -37,33 +34,34 @@ class NewPostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        submitButton.isEnabled = false
-        let geocoder = CLGeocoder()
-        geocoder.reverseGeocodeLocation(CLLocation(latitude: postLatitude, longitude: postLongitude)) { (placemarkArray, error) in
-            if error == nil {
-                let pm = placemarkArray?[0]
-                self.titleLabel.text = pm?.thoroughfare
-            }
-        }
-        self.view.backgroundColor = color.lighterColor(0.3)
-        
         titleTextField.delegate = self
         messageTextField.delegate = self
         
+        submitButton.isEnabled = false
+        self.view.backgroundColor = color.lighterColor(0.3)
+        
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(CLLocation(latitude: postLatitude, longitude: postLongitude)) { (placemarkArray, error) in
+            if error == nil {
+                let userLocation = placemarkArray?[0]
+                self.titleLabel.text = userLocation?.thoroughfare
+            }
+        }
     }
     
     
     @IBAction func submitButtonPressed(_ sender: Any) {
         
-        reachability.whenReachable = { reachable in
+        reachabilityNetworkConnection.whenReachable = { reachable in
             DispatchQueue.main.async {
                 if self.firebaseController.enforceNewPostRateLimit() {
                     self.activityIndicator.startAnimating()
                     
                     let dictionaryOfNewTopicValues = ["username": self.userProfile.username!, "twitterID": self.userProfile.twitterid!, "topicTitle": self.titleTextField.text!, "topicMessage": self.messageTextField.text!, "color": self.color.hexCode, "latitude": self.postLatitude, "longitude": self.postLongitude] as [String : Any]
                     
-                    self.firebaseController.createNewTopicPostOnFirebase(dictionaryOfNewPostValues: dictionaryOfNewTopicValues, baseView: self, completionHandler: { (delayed) in
-                        if delayed == false {
+                    self.firebaseController.createNewTopicPostOnFirebase(dictionaryOfNewPostValues: dictionaryOfNewTopicValues, baseView: self, completionHandler: { (postWasInitiallyDelayed) in
+                        
+                        if postWasInitiallyDelayed == false {
                             if let navController = self.navigationController, navController.viewControllers.count >= 2 {
                                 let viewController = navController.viewControllers[navController.viewControllers.count - 2] as! MapViewController
                                 viewController.fromNewPost = true
@@ -74,19 +72,22 @@ class NewPostViewController: UIViewController {
                         }
                     } )
                 } else {
+                    
                     self.errorAlertController.displayAlert(title: "Slow Down!", message: "Please wait 20 seconds between posting", view: self)
-                    self.reachability = Reachability()!
+                    
+                    //Resets the reachability library
+                    self.reachabilityNetworkConnection = Reachability()!
                 }
             }
         }
-        reachability.whenUnreachable = { _ in
+        reachabilityNetworkConnection.whenUnreachable = { _ in
             DispatchQueue.main.async {
                 self.errorAlertController.displayAlert(title: "Connection Issue", message: "There was an issue with your connection, we'll keep trying to post though!", view: self)
             }
         }
         
         do {
-            try reachability.startNotifier()
+            try reachabilityNetworkConnection.startNotifier()
         } catch {
             print("Unable to start notifier")
         }
